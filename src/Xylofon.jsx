b857f999e94f860ce10bc30469de043588bdaa8e
Veronika Bendova <svechovaver@gmail.com>
2023-07-31 11:53:47 +0200

    Task done

--- src/Xylofon.jsx
+++ src/Xylofon.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import { zahraj } from './zahraj'
+import React from 'react';
+import { zahraj } from './zahraj';
+import { useState } from 'react';
 
 // 1. Nahraď jednotlivé tlačítka/kameny za výpis pole pomocí `.map`. Použij následující pole `kameny`.
-const kameny = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
+const kameny = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
 
 // 2. Přichystej si stavovou proměnnou `nadpis`, ve které jako výchozí hodnotu nastav „Xylofon“ a proměnnou vypiš uvnitř tagu `<h1>`.
 // 3. Po kliknutí na jakýkoliv kámen změň hodnotu nadpisu na text „Cink!“. Zkontroluj, že se nadpis opravdu změní.
@@ -11,20 +12,29 @@ const kameny = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
 // 6. Zapni si v počítači zvuk a xylofon otestuj.
 
 export const Xylofon = () => {
-	return (
-		<>
-			<h1>Xylofon</h1>
-			<div className="xylofon">
-				<button type="button" className="kamen">
-					C
-				</button>
-				<button type="button" className="kamen">
-					D
-				</button>
-				<button type="button" className="kamen">
-					E
-				</button>
-			</div>
-		</>
-	)
-}
+  const [nadpis, setNadpis] = useState('Xylofon');
+
+  const handleClick = (event) => {
+    const tone = event.target.textContent;
+    setNadpis(tone);
+    zahraj(tone);
+  };
+
+  return (
+    <>
+      <h1>{nadpis}</h1>
+      <div className="xylofon">
+        {kameny.map((kamen, index) => (
+          <button
+            key={index}
+            type="button"
+            className="kamen"
+            onClick={handleClick}
+          >
+            {kamen}
+          </button>
+        ))}
+      </div>
+    </>
+  );
+};
